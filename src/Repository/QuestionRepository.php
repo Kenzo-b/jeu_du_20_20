8765a4b1b39ec05a9004cbc24377d46cbe7e0588
kenzo-b <contact@kbriche.fr>
2024-09-13 13:18:33 +0200

    make getRandomQuestionByDifficulty

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -4,35 +4,49 @@ namespace kenzo\Jeu20\Repository;
 
 use kenzo\Jeu20\Entity\Question;
 
-$questions = [
-    (new Question())->setContentText(
-        'Qu\'est-ce qu\'une variable en programmation ?'
-    )->setLevel(1),
-    (new Question())->setContentText(
-        'Quelle est la différence entre une boucle "for" et "while" ?'
-    )->setLevel(1),
-    (new Question())->setContentText(
-        'Qu\'est-ce qu\'un pointeur en langage C ?'
-    )->setLevel(1),
-    (new Question())->setContentText(
-        'Expliquez la différence entre HTTP et HTTPS.'
-    )->setLevel(2),
-    (new Question())->setContentText(
-        'Qu\'est-ce que le polymorphisme en programmation orientée objet ?'
-    )->setLevel(2),
-    (new Question())->setContentText(
-        'Définissez ce qu\'est un algorithme de tri rapide (quick sort).'
-    )->setLevel(2),
-    (new Question())->setContentText(
-        'Qu\'est-ce qu\'un objet en programmation orientée objet ?'
-    )->setLevel(1),
-    (new Question())->setContentText(
-        'Comment fonctionne le garbage collector en Java ?'
-    )->setLevel(2),
-    (new Question())->setContentText(
-        'Quelle est la différence entre un tableau associatif et un tableau indexé en PHP ?'
-    )->setLevel(1),
-    (new Question())->setContentText(
-        'Expliquez ce qu\'est un "cookie" et comment il est utilisé en développement web.'
-    )->setLevel(2),
-];
+class QuestionRepository
+{
+    private function getRandomQuestionByDifficulty(int $level): Question
+    {
+        $questions = [
+            (new Question())->setContentText(
+                'Qu\'est-ce qu\'une variable en programmation ?'
+            )->setLevel(1),
+            (new Question())->setContentText(
+                'Quelle est la différence entre une boucle "for" et "while" ?'
+            )->setLevel(1),
+            (new Question())->setContentText(
+                'Qu\'est-ce qu\'un pointeur en langage C ?'
+            )->setLevel(1),
+            (new Question())->setContentText(
+                'Expliquez la différence entre HTTP et HTTPS.'
+            )->setLevel(2),
+            (new Question())->setContentText(
+                'Qu\'est-ce que le polymorphisme en programmation orientée objet ?'
+            )->setLevel(2),
+            (new Question())->setContentText(
+                'Définissez ce qu\'est un algorithme de tri rapide (quick sort).'
+            )->setLevel(2),
+            (new Question())->setContentText(
+                'Qu\'est-ce qu\'un objet en programmation orientée objet ?'
+            )->setLevel(1),
+            (new Question())->setContentText(
+                'Comment fonctionne le garbage collector en Java ?'
+            )->setLevel(2),
+            (new Question())->setContentText(
+                'Quelle est la différence entre un tableau associatif et un tableau indexé en PHP ?'
+            )->setLevel(1),
+            (new Question())->setContentText(
+                'Expliquez ce qu\'est un "cookie" et comment il est utilisé en développement web.'
+            )->setLevel(2),
+        ];
+
+        $questionsAtLevel = [];
+        foreach ($questions as $question) {
+            if (!in_array($question->getLevel() === $level, $questions)) throw new \Exception("il n'y a pas de question de niveau $level");
+            if ($question->getLevel() === $level) ($questionsAtLevel[] = $question);
+        }
+        return array_rand($questionsAtLevel,1)[0];
+    }
+
+}
