10da9355d011fd7a245545051e0daf64b5233403
kenzo-b <contact@kbriche.fr>
2024-09-13 13:50:40 +0200

    make question view

---
+++ src/Templates/question/show_question.php
@@ -0,0 +1,24 @@
+<html lang="fr">
+    <head>
+        <title>jeu du 20/20</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+            <h1>jeu du 20/20</h1>
+        </header>
+        <section>
+            <article>vous jouer pour la note de 1/20</article>
+            <article>
+                <h2>question posée :</h2>
+                <p>qu'est-ce qu'un pointeur en c?</p>
+            </article>
+            <article>question proposé le : 13-09-2024 13:46:06</article>
+        </section>
+        <footer>
+            2024 - Briche Kenzo
+        </footer>
+    </body>
+</html>
+
+
