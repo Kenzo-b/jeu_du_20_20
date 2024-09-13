24f42c3348b5e4fa2215c64e6b7839e02b5fa2d9
kenzo-b <contact@kbriche.fr>
2024-09-13 14:11:46 +0200

    add showSimpleQuestionTemp

--- src/ Controller/QuestionController.php
+++ src/ Controller/QuestionController.php
@@ -2,6 +2,16 @@
 
 namespace kenzo\Jeu20\Controller;
 
-class QuestionController {
+use kenzo\Jeu20\Repository\QuestionRepository;
 
+class QuestionController {
+    public function showSimpleQuestionTemp (int $difficulty) {
+        $params = [];
+        $question = (new QuestionRepository())->findRandomQuestionByDifficulty($difficulty);
+        $params[] = $question;
+        ob_start();
+        extract($params);
+        require_once '../templates/question/show_question.php';
+        return ob_get_clean();
+    }
 }
