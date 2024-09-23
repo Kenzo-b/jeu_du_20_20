372a4c05532893197bccaf26e83e6c4b121148bb
kenzo-b <contact@kbriche.fr>
2024-09-23 15:30:15 +0200

    add the get function for js and css

--- viewDefinitions/show_question_definition_temp.php
+++ viewDefinitions/show_question_definition_temp.php
@@ -4,4 +4,6 @@ use kenzo\Jeu20\Utils\ViewRender;
 $view_header = ViewRender::getContentView("common/header");
 $view_main = ViewRender::getContentView("question/new_show_question");
 $view_footer = ViewRender::getContentView("common/footer");
+$cssFiles = ViewRender::getCssLinks([]);
+$jsFiles = ViewRender::getJsScripts([]);
 include ViewRender::getValidatedViewPath('base');
