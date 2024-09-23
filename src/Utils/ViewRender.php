d10d5102b25f5d4c3c778d8a9aadbaf7f8e0b0c6
kenzo-b <contact@kbriche.fr>
2024-09-23 14:30:13 +0200

    renderer made

---
+++ src/Utils/ViewRender.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace kenzo\Jeu20\utils;
+
+class ViewRender
+{
+    public const string VIEW_DEFINITIONS_DIRECTORY_PATH = '/../../viewDefinitions/';
+
+    public static function getValidatedViewDefinitionPath(string $view): string
+    {
+        $viewDefinitionToCheck = __DIR__.self::VIEW_DEFINITIONS_DIRECTORY_PATH.$view.'.php';
+        if (!file_exists($viewDefinitionToCheck)) {
+            throw new \Exception("Le fichier de définition de la vue à rendre n'existe pas : " .$viewDefinitionToCheck);
+        }
+        return $viewDefinitionToCheck;
+    }
+
+    public static function renderFromViewDefinition(string $viewDefinition, $data = []): void
+    {
+        $viewDefinitionPath = self::getValidatedViewDefinitionPath($viewDefinition);
+        include $viewDefinitionPath;
+    }
+}
