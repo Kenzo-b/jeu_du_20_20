0e9bb1597aae70ec0242d30fd99ab51d0642f0e2
kenzo-b <contact@kbriche.fr>
2024-09-23 12:24:27 +0200

    make the common view file

---
+++ templates/common/header.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @var \kenzo\Jeu20\Entity\Question $question
+ */
+?>
+<h2>Vous jouez pour la note de <span><?php echo $question->getLevel()? $question->getLevel() : ''?></span>/20</h2>
