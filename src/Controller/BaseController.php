e2cc135ade6305d017580bf8e9ec7741378557b2
kenzo-b <contact@kbriche.fr>
2024-09-23 12:38:10 +0200

    BaseController class made

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace kenzo\Jeu20\Controller;
+
+class BaseController
+{
+    public static function renderViewTemp(
+        string $viewDefinition,
+        array $data = []
+    ): void {
+    }
+}
