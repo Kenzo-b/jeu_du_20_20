659bebf6906354d9776b23ed18bb4bc89e963397
kenzo-b <contact@kbriche.fr>
2024-09-30 09:18:11 +0200

    add the modif for the answer

--- templates/question/new_show_question.php
+++ templates/question/new_show_question.php
@@ -1,17 +1,29 @@
+<!--fichier templates\question\new_show_question.php-->
 <?php
 /**
- * @var \kenzo\Jeu20\Entity\Question $question
+ * @var kenzo\Jeu20\Entity\Question $question
  */
 ?>
 <article>
     <header>
-        <h2>Vous jouez pour la note de <span><?php echo $question->getLevel()? $question->getLevel() : ''?></span>/20</h2>
+        <h2>Vous jouez pour la note de <span><?php echo $question->getLevel(
+                ); ?></span>/20</h2>
     </header>
     <section>
         <h3>Question posée :</h3>
         <div><?php echo htmlspecialchars($question->getContentText()) ?></div>
+        <h3>Réponses :</h3>
+        <div>
+            <ol>
+                <?php
+                foreach ($question->getAnswers() as $answer) : ?>
+                    <li><?php echo htmlspecialchars($answer->getContentText()) ?></li>
+                <?php endforeach;?>
+            </ol>
+        </div>
     </section>
     <section>
-        <p>question proposée le : <?php echo htmlspecialchars($question->getCreatedAt()->format('Y-m-d H:i:s')) ?></p>
+        <p>Question proposée le : <?php echo htmlspecialchars(
+                $question->getCreatedAt()->format('Y-m-d H:i:s')) ?></p>
     </section>
 </article>
