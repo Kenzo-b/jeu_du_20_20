24a447d92d2efef7378ff70acbb34e8ebeef5317
kenzo-b <contact@kbriche.fr>
2024-09-30 09:17:23 +0200

    delete unused func

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -6,11 +6,6 @@ use kenzo\Jeu20\utils\ViewRender;
 
 class BaseController
 {
-    public static function renderViewTemp(string $viewDefinition, array $data = []): void
-    {
-        extract($data);
-        include __DIR__.'/../../viewDefinitions/'.$viewDefinition.'.php';
-    }
 
     public static function renderFromViewDefinition(string $viewDefinition, $data = []): void
     {
